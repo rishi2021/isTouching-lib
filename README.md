5550d32b18f97c555f518747d0cae77c8ac02f7e
rishi2021 <63649201+rishi2021@users.noreply.github.com>
2020-04-23 17:30:14 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# isTouching-lib
+# bounceOffAlgorithm
+Algorithm to bounce off two objects
---
+++ sketch.js
@@ -0,0 +1,47 @@
+var fixedRect, movingRect,rect1,rect2;
+
+function setup() {
+  createCanvas(1200,800);
+  fixedRect = createSprite(400, 100, 50, 80);
+  fixedRect.shapeColor = "green";
+  fixedRect.debug = true;
+  movingRect = createSprite(400, 800,80,30);
+  movingRect.shapeColor = "green";
+  movingRect.debug = true;
+  rect1 = createSprite(200,100,50,50);
+  rect1.shapeColor = "green";
+  rect2 = createSprite(300,100,50,50);
+  rect2.shapeColor = "green";
+  //movingRect.velocityY = -5;
+  //fixedRect.velocityY = +5;
+}
+
+function draw() {
+  background(0,0,0);  
+  movingRect.x = World.mouseX;
+  movingRect.y = World.mouseY;
+  if (movingRect.x - fixedRect.x < fixedRect.width/2 + movingRect.width/2
+      && fixedRect.x - movingRect.x < fixedRect.width/2 + movingRect.width/2) {
+    movingRect.velocityX = movingRect.velocityX * (-1);
+    fixedRect.velocityX = fixedRect.velocityX * (-1);
+  }
+  if (movingRect.y - fixedRect.y < fixedRect.height/2 + movingRect.height/2
+    && fixedRect.y - movingRect.y < fixedRect.height/2 + movingRect.height/2){
+    movingRect.velocityY = movingRect.velocityY * (-1);
+    fixedRect.velocityY = fixedRect.velocityY * (-1);
+  }
+  if ( isTouching(movingRect,rect1)){
+    movingRect.shapeColor = "blue";
+    rect1.shapeColor = "blue";
+
+  }
+  else {
+    movingRect.shapeColor = "green";
+    rect1.shapeColor = "green";
+
+  }
+  
+  drawSprites();
+}
+
+
